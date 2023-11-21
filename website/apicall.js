8177ac2b11eccf1c9773d476b3602e9719a603fc
Zachary <zderish@gmail.com>
2023-11-20 19:43:13 -0500

    Update UI

--- website/apicall.js
+++ website/apicall.js
@@ -18,14 +18,27 @@ fetch('http://127.0.0.1:5000/recruiting/year=2024')
   });
 
 function create_table(data) {
-    for (let i = 0; i < 2000; i++) {
-        let row = table.insertRow(i+1);
-        let name = row.insertCell(0);
-        let position = row.insertCell(1);
-        let ranking = row.insertCell(2);
+  const TABLE_HEADERS = ["Name", "Position", "Ranking", "Commited To"];
+  let row = table.insertRow(0);
+  // Create header
+  for (let i = 0; i < TABLE_HEADERS.length; i++) {
+    let th = document.createElement("th");
+    th.textContent = TABLE_HEADERS[i];
+    row.append(th); 
+  }
+  
 
-        name.textContent = `${data[i].name}`
-        position.textContent = `${data[i].position}`
-        ranking.textContent = `${data[i].ranking}`;
-    }
+
+  for (let i = 0; i < 2000; i++) {
+    let row = table.insertRow(i + 1);
+    let name = row.insertCell(0);
+    let position = row.insertCell(1);
+    let ranking = row.insertCell(2);
+    let college = row.insertCell(3);
+
+    name.textContent = `${data[i].name}`;
+    position.textContent = `${data[i].position}`;
+    ranking.textContent = `${data[i].ranking}`;
+    college.textContent = `${data[i].school}`;
+  }
 }
