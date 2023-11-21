aad3e7e316a7c47b5141018cec13f00488c5eccb
Zachary <zderish@gmail.com>
2023-11-21 09:51:11 -0500

    Add 2022 and 2023 classes

--- website/apicall.js
+++ website/apicall.js
@@ -1,6 +1,9 @@
 let table = document.getElementById("table");
+let year = document.getElementById("year")
+let yearString = year.innerText.split(" ")[0]
+console.log(yearString)
 
-fetch('http://127.0.0.1:5000/recruiting/year=2024') 
+fetch(`http://127.0.0.1:5000/recruiting/year=${yearString}`) 
   .then(response => { 
     if (response.ok) { 
         return response.json(); // Parse the response data as JSON 
@@ -18,7 +21,7 @@ fetch('http://127.0.0.1:5000/recruiting/year=2024')
   });
 
 function create_table(data) {
-  const TABLE_HEADERS = ["Name", "Position", "Ranking", "Commited To"];
+  const TABLE_HEADERS = ["Name", "Position","Height", "Weight", "Stars", "Ranking", "Commited To"];
   let row = table.insertRow(0);
   // Create header
   for (let i = 0; i < TABLE_HEADERS.length; i++) {
@@ -33,11 +36,17 @@ function create_table(data) {
     let row = table.insertRow(i + 1);
     let name = row.insertCell(0);
     let position = row.insertCell(1);
-    let ranking = row.insertCell(2);
-    let college = row.insertCell(3);
+    let height = row.insertCell(2);
+    let weight = row.insertCell(3);
+    let stars = row.insertCell(4);
+    let ranking = row.insertCell(5);
+    let college = row.insertCell(6);
 
     name.textContent = `${data[i].name}`;
-    position.textContent = `${data[i].position}`;
+    position.textContent = `${data[i].position}`; 
+    height.textContent = `${data[i].height}`;
+    weight.textContent = `${data[i].weight}`;
+    stars.textContent = `${data[i].stars}`;
     ranking.textContent = `${data[i].ranking}`;
     college.textContent = `${data[i].school}`;
   }
