4dcfd2152b70b68651455fc90591b3347f4e0909
Zachary <zderish@gmail.com>
2024-03-17 12:56:14 -0400

    Delete venv

--- venv/application.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-application = Flask(__name__)
-
-@application.route('/')
-def hello_world():
-  return "Hello world!"
-
-if __name__ == "__main__":
-  application.run(debug=True)
