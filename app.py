6b31dcfc92ea0ba0659b55ac986f1a0389c9b464
Zachary <zderish@gmail.com>
2024-06-13 14:19:00 -0400

    Add team recruiting endpoint

--- app.py
+++ app.py
@@ -14,20 +14,23 @@ mydb = mysql.connector.connect(
 
 @app.route('/')
 def hello_world():
-    return 'This is a test 1.'
-
-@app.route('/path1')
-def hello_path1():
-    return f"Hello test path1!"
-
-@app.route('/path2')
-def hello_path2():
-    return "Hello test path2!"
+    return 'Endpoint: recruiting/year=<year>'
 
 @app.route('/recruiting/year=<year>')
 def player_recruiting(year):
     mycursor = mydb.cursor(dictionary=True)
     query = "SELECT * FROM recruiting" + year
+    mycursor.execute(query)
+    data = mycursor.fetchall()
+    json_data = json.dumps(data)
+    response = make_response(json_data)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
+
+@app.route('recruiting/year=<year>/team=<team>')
+def team_recruiting(year, team):
+    mycursor = mydb.cursor(dictionary=True)
+    query = "SELECT * FROM recruiting" + year + " WHERE team = " + team
     mycursor.execute(query)
     data = mycursor.fetchall()
     json_data = json.dumps(data)
